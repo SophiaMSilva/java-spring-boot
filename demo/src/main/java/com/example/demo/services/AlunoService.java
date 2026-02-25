6df298980185408d718c55800912cc9e3070d77b
unknown <sophiamachadosilva07@gmail.com>
2026-02-24 22:00:54 -0300

    service criado

---
+++ demo/src/main/java/com/example/demo/services/AlunoService.java
@@ -0,0 +1,28 @@
+package com.example.demo.services;
+
+
+import com.example.demo.models.AlunoModel;
+import com.example.demo.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public AlunoModel criarAluno(AlunoModel alunoModel){
+        return alunoRepository.save(alunoModel);
+    }
+
+    public List<AlunoModel> findAll(){
+        return alunoRepository.findAll();
+    }
+
+    public void deletarAluno(Long id){
+        alunoRepository.deleteById(id);
+    }
+}
